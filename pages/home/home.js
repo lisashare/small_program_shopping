1a5dfedc4fbd9c733c7fb261c4e70f5f1aec9d07
lisashare <lisa_zhu2012@foxmail.com>
2018-06-04 17:36:47 +0800

    首页的商品列表完成，实现了下拉刷新

--- pages/home/home.js
+++ pages/home/home.js
@@ -25,7 +25,8 @@ Page({
       autoplay: true,        //是否自动切换
       interval: 3000,        //自动切换时间间隔 3s
       duration: 1000,        //滑动动画时长 1s
-      
+      isHideLoadMore: false,
+
     // navItems
     navItems: [
       {
@@ -50,9 +51,114 @@ Page({
         typeId: 3,
         name: '推荐好友',
         url: 'bill',
-        imageurl: '../../images/navItems/home_icon_frends.png'
+        imageurl: '../../images/navItems/home_icon_friends.png'
       }
     ],
+    hot_products:
+    [
+      {
+        product_id: 1,
+        imageurl: 'http://mz.djmall.xmisp.cn/files/banner/20161222/148237182618.png',
+        html: "http://mz.djmall.xmisp.cn/files/activity/20161216/5853a0137573e84/mz2_ajax/index.html"
+      },
+      {
+        product_id: 2,
+        imageurl: 'http://mz.djmall.xmisp.cn/files/banner/20161202/148066038440.png',
+        html: "http://mz.djmall.xmisp.cn/files/activity/20161208/584926f0017d874/mz1/index.html"
+      }
+    ],
+    newgoods_head_url:"http://mz.djmall.xmisp.cn/files/banner/20161202/148066062976.jpg",
+    // 新品上架
+    goodsItems: [
+      {
+        goodId: 0,
+        name: '兰蔻小黑瓶',
+        url: 'bill',
+        imageurl: 'http://mz.djmall.xmisp.cn/files/product/20161201/148057921620.jpg',
+        newprice: "￥200.00",
+        oldprice: "￥300.00",
+      },
+      {
+        goodId: 1,
+        name: '兰蔻清莹柔肤爽肤水',
+        url: 'bill',
+        imageurl: 'http://mz.djmall.xmisp.cn/files/product/20161201/148057953326.jpg',
+        newprice: "￥120.00",
+        oldprice: "￥200.00",
+      },
+      {
+        goodId: 2,
+        name: '倩碧水嫩保湿精华面霜',
+        url: 'bill',
+        imageurl: 'http://mz.djmall.xmisp.cn/files/product/20161201/148058228431.jpg',
+        newprice: "￥320.00",
+        oldprice: "￥400.00",
+      },
+      {
+        goodId: 3,
+        name: '特效润肤露',
+        url: 'bill',
+        imageurl: 'http://mz.djmall.xmisp.cn/files/product/20161201/14805828016.jpg',
+        newprice: "￥30.00",
+        oldprice: "￥80.00",
+      },
+      {
+        goodId: 4,
+        name: '倩碧焕妍活力精华露',
+        url: 'bill',
+        imageurl: 'http://mz.djmall.xmisp.cn/files/product/20161201/148058301941.jpg',
+        newprice: "￥30.00",
+        oldprice: "￥80.00",
+      }, {
+        goodId: 5,
+        name: '日本资生堂洗颜',
+        url: 'bill',
+        imageurl: 'http://mz.djmall.xmisp.cn/files/product/20161201/148058328876.jpg',
+        newprice: "￥30.00",
+        oldprice: "￥80.00",
+      }
+    ],
+    recommends: [
+      {
+        goodId: 7,
+        name: 'OLAY玉兰油精油沐浴露玫瑰滋养二合一450ml',
+        url: 'bill',
+        imageurl: 'http://mz.djmall.xmisp.cn/files/product/20161213/148162245074.jpg',
+        newprice: "￥36.60",
+        oldprice: "￥40.00",
+      },
+      {
+        goodId: 10,
+        name: '兰蔻玫瑰清滢保湿柔肤水嫩肤水化妆水400ml补水保湿温和不刺激',
+        url: 'bill',
+        imageurl: 'http://mz.djmall.xmisp.cn/files/product/20161201/148057937593.jpg',
+        newprice: "￥30.00",
+        oldprice: "￥80.00",
+      }, {
+        goodId: 11,
+        name: 'Lancome/兰蔻清莹柔肤爽肤水/粉水400ml补水保湿玫瑰水化妆水',
+        url: 'bill',
+        imageurl: 'http://mz.djmall.xmisp.cn/files/product/20161201/148057953326.jpg',
+        newprice: "￥30.00",
+        oldprice: "￥80.00",
+      },
+      {
+        goodId: 12,
+        name: '美国CLINIQUE倩碧黄油无油/特效润肤露125ml',
+        url: 'bill',
+        imageurl: 'http://mz.djmall.xmisp.cn/files/product/20161201/14805828016.jpg',
+        newprice: "￥239.00",
+        oldprice: "￥320.00",
+      },
+      {
+        goodId: 13,
+        name: '法国LANCOME兰蔻柔皙轻透隔离防晒乳霜50ML2017年3月到期',
+        url: 'bill',
+        imageurl: 'http://mz.djmall.xmisp.cn/files/product/20161201/148058014894.jpg',
+        newprice: "￥130.00",
+        oldprice: "￥180.00",
+      },
+    ]
   },
   navbarTap (e) {
     console.log(e);
@@ -98,14 +204,71 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+    //在标题栏中显示加载
+    wx.showNavigationBarLoading() 
+    //模拟加载
+    setTimeout(function(){
+      //complete
+      wx.hideNavigationBarLoading() //完成停止加载
+      wx.stopPullDownRefresh();     //停止下拉刷新
+    },1500)
+    
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+    setTimeout(() => {
+
+     let recommends = [
+        {
+          goodId: 7,
+          name: 'OLAY玉兰油精油沐浴露玫瑰滋养二合一450ml',
+          url: 'bill',
+          imageurl: 'http://mz.djmall.xmisp.cn/files/product/20161213/148162245074.jpg',
+          newprice: "￥36.60",
+          oldprice: "￥40.00",
+        },
+        {
+          goodId: 10,
+          name: '兰蔻玫瑰清滢保湿柔肤水嫩肤水化妆水400ml补水保湿温和不刺激',
+          url: 'bill',
+          imageurl: 'http://mz.djmall.xmisp.cn/files/product/20161201/148057937593.jpg',
+          newprice: "￥30.00",
+          oldprice: "￥80.00",
+        }, {
+          goodId: 11,
+          name: 'Lancome/兰蔻清莹柔肤爽肤水/粉水400ml补水保湿玫瑰水化妆水',
+          url: 'bill',
+          imageurl: 'http://mz.djmall.xmisp.cn/files/product/20161201/148057953326.jpg',
+          newprice: "￥30.00",
+          oldprice: "￥80.00",
+        },
+        {
+          goodId: 12,
+          name: '美国CLINIQUE倩碧黄油无油/特效润肤露125ml',
+          url: 'bill',
+          imageurl: 'http://mz.djmall.xmisp.cn/files/product/20161201/14805828016.jpg',
+          newprice: "￥239.00",
+          oldprice: "￥320.00",
+        },
+        {
+          goodId: 13,
+          name: '法国LANCOME兰蔻柔皙轻透隔离防晒乳霜50ML2017年3月到期',
+          url: 'bill',
+          imageurl: 'http://mz.djmall.xmisp.cn/files/product/20161201/148058014894.jpg',
+          newprice: "￥130.00",
+          oldprice: "￥180.00",
+        },
+      ];
+      recommends = this.data.recommends.concat(recommends);
+
+      this.setData({
+        isHideLoadMore: true,
+        recommends:recommends
+      })
+    }, 1000)
   },
 
   /**
